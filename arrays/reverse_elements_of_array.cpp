04dc74a8b67b2a16894e192b59c57874629501c1
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-24 19:43:06 +0530

    Formatted : reverse_elements_of_array.cpp

--- arrays/reverse_elements_of_array.cpp
+++ arrays/reverse_elements_of_array.cpp
@@ -9,31 +9,31 @@ struct Array
 };
 
 // Function to print array elements
-void display(struct Array arg)
+void display(struct Array arr)
 {
-    for (int i = 0; i < arg.length;i++)
-        cout<<arg.A[i]<<" ";
+    for (int i = 0; i < arr.length; i++)
+        cout << arr.A[i] << " ";
 }
 
 /* Function to reverse array element using auxiliary array */
 void reverse_1(struct Array *arr)
 {
-    int i,j;
+    int i, j;
     int b[arr->length];
 
-    for (i=0, j=arr->length-1; j>=0; i++,j--)
+    for (i = 0, j = arr->length - 1; j >= 0; i++, j--)
         b[i] = arr->A[j];
 
-    for (i=0; i<arr->length; i++)
+    for (i = 0; i < arr->length; i++)
         arr->A[i] = b[i];
 }
 
 /* Function to reverse array element using swap */
 void reverse_2(struct Array *arr)
 {
-    int i,j,temp;
+    int i, j, temp;
 
-    for (i=0, j=arr->length-1; i<j; i++,j--)
+    for (i = 0, j = arr->length - 1; i < j; i++, j--)
     {
         temp = arr->A[i];
         arr->A[i] = arr->A[j];
@@ -44,26 +44,27 @@ void reverse_2(struct Array *arr)
 int main()
 {
     struct Array arr;
-    
-    cout<<"Enter the Array Size : ";
-    cin>>arr.size;
-    arr.A = new int[arr.size];    
-    
-    cout<<"How many values do you want to enter : ";
-    cin>>arr.length;
 
+    cout << "Enter the array size : ";
+    cin >> arr.size;
+    arr.A = new int[arr.size]{0};
+
+    cout << "How many values do you want to enter : ";
+    cin >> arr.length;
+
+    cout << "Enter array values : ";
     for (int i = 0; i < arr.length; i++)
-        cin>>arr.A[i];
+        cin >> arr.A[i];
 
-    cout<<"Array Elements : ";
+    cout << "Array Elements : ";
     display(arr);
 
-    reverse_1(&arr);    //Method 1: Reverse using auxiliary array
-     cout<<"\n\nMethod 1: Reverse using auxiliary array \nReversed Array Element: ";
+    reverse_1(&arr); //Method 1: Reverse using auxiliary array
+    cout << "\n\nMethod 1: Reverse using auxiliary array \nReversed Array Element: ";
     display(arr);
 
-    reverse_2(&arr);     //Method 2: Reverse using swaping elements
-     cout<<"\n\nMethod 2: Reverse using swaping elements \nReversed Array Element: ";
+    reverse_2(&arr); //Method 2: Reverse using swaping elements
+    cout << "\n\nMethod 2: Reverse using swaping elements \nReversed Array Element: ";
     display(arr);
 
     return 0;
@@ -72,7 +73,7 @@ int main()
 /*
 Input : Enter the Array Size : 10
         How many values do you want to enter : 6
-        10
+        Enter array values :10
         20
         30
         40
