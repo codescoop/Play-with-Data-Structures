4e4602fb9a86f71ed76e99a3df1cbd6b9dad09cd
Yogesh <yogesh7132@gmail.com>
2022-01-04 16:05:13 +0530

    Update rotate function

--- 12_algorithms_stl/05_rotate_and_next_permutation.cpp
+++ 12_algorithms_stl/05_rotate_and_next_permutation.cpp
@@ -30,12 +30,10 @@ void print_vector(vector<int> v){
 
 int main()
 {
-    vector<int> v{1,2,3,4};
+    vector<int> v{1,2,3,4,5,6,7,8,9,10,11};
 
     int arr[] = {10,20,30,40,50};
     int size = sizeof(arr)/sizeof(int);
-    int mid = size/2;
-
 
     /*  Rotate Function: It rotates the order of the elements in the range [first,last], 
                          in such a way that the element pointed by middle becomes the 
@@ -44,28 +42,35 @@ int main()
         Syntax :  void rotate(ForwardIterator first, ForwardIterator middle, ForwardIterator last)
     */
 
-    // using rotate() on arrays
+    // using rotate() on arrays (towards Left)
     cout << "Array: ";
     print_array(arr,size);
 
-    rotate(arr, arr+mid, arr+size);                // rotate
-
-    cout << "After Rotate: ";
+    rotate(arr, arr+2, arr+size);         // rotate array twice (towards left)
+    cout << "After Left Rotate (twice): ";
     print_array(arr,size);
     cout << endl;
 
+    // using rotate() on arrays (towards Right)
+    cout << "Array: ";
+    print_array(arr,size);
+
+    rotate(arr, arr+(size-2), arr+size);    // rotate array twice (towards right)
+    cout << "After Right Rotate (twice): ";
+    print_array(arr,size);
+    cout << endl;
 
     // using rotate() on vectors
     cout << "Vector: ";
     print_vector(v);
 
-    rotate(v.begin(), v.begin()+mid, v.end());     // rotate
-    rotate(v.begin(), v.begin()+mid, v.end());     // rotate again
-    rotate(v.begin(), v.begin()+mid, v.end());     // rotate again
-    
-    cout << "After Rotate: ";
+    rotate(v.begin(), v.begin()+4, v.end());  // rotate vector 4 times (towards left)
+
+    cout << "After Left Rotate (4 times): ";
     print_vector(v);
-    cout << endl;
+    cout << "\n\n";
+
+
 
     /*  Next Permutation  It is used to rearrange the elements in the range [first, last) 
                           into the next lexicographically greater permutation.
@@ -124,23 +129,28 @@ int main()
 /* 
 OUTPUT: 
 
-    Array: 10 20 30 40 50 
-    After Rotate: 30 40 50 10 20 
+Array: 10 20 30 40 50 
+After Left Rotate (twice): 30 40 50 10 20 
 
-    Vector: 1 2 3 4 
-    After Rotate: 3 4 1 2 
+Array: 30 40 50 10 20 
+After Right Rotate (twice): 10 20 30 40 50 
 
-    Array: 10 20 30 40 
-    After next permutation: 10 20 40 30 
+Vector: 1 2 3 4 5 6 7 8 9 10 11 
+After Left Rotate (4 times): 5 6 7 8 9 10 11 1 2 3 4 
 
-    Vector: 1 2 3 
-    After next permutation: 1 3 2
 
-    All permutation value of Vector v2: 1 2 3 
-    v2: 1 2 3 
-    v2: 1 3 2 
-    v2: 2 1 3 
-    v2: 2 3 1 
-    v2: 3 1 2 
-    v2: 3 2 1 
+
+Array: 10 20 30 40 
+After next permutation: 10 20 40 30 
+
+Vector: 1 2 3 
+After next permutation: 1 3 2 
+
+All permutation value of Vector v2: 1 2 3 
+v2: 1 2 3 
+v2: 1 3 2 
+v2: 2 1 3 
+v2: 2 3 1 
+v2: 3 1 2 
+v2: 3 2 1 
 */
