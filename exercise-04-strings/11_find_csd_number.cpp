c76356a389d84599d39f4e64c2d040388b746f06
Yogesh <yogesh7132@gmail.com>
2022-01-07 19:05:57 +0530

    updated approach

--- exercise-04-strings/11_find_csd_number.cpp
+++ exercise-04-strings/11_find_csd_number.cpp
@@ -37,235 +37,135 @@
 */
 
 #include <iostream>
-#include <algorithm>
-#include <string>
-#define MAX 2000
+// #include <bits/stdc++.h>
+
 using namespace std;
 
-// comparator function
-bool sortOrder(int a, int b){
-    return a>b;
+// function to convert string to long
+long stringtolong(string s)
+{
+    long idx = 0;
+    long num = 0;
+    while(s[idx] >= '0' && s[idx] <= '9')
+    {
+        num = num * 10 + (s[idx] - '0');
+        idx++;
+    }
+    return num;
 }
 
-// check if number is CSD number
-bool isCSDNumber(int num){
-    if(num == 0 || num==1){
+
+// function to check CSD Number
+bool is_cb_number(string s)
+{   
+    // coverting string into long long using STL function
+    long long num = stoll(s);
+
+    // check first condtion of CSD number
+    if (num == 0 || num == 1) {
         return false;
     }
-    int i=2;
-    while(i <= num-1){
-        if(num%i == 0){
+
+    // check second condtion of CSD number
+    int arr[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    for (int i = 0; i < size; i++) {
+        if (num == arr[i]) {
+            return true;
+        }
+    }
+    // check third condtion of CSD number
+    for (int i = 0; i < size; i++) {
+        if (num % arr[i] == 0) {
             return false;
         }
-        i++;
     }
     return true;
 }
 
-// funtion to get all sub arrays
-int getSubArrays(int arr[], int size, int subArray[]){
-    int count=0;
-    for(int i=0; i<=size-1; i++){
-        for(int j=i; j<=size-1; j++){
-            int num = 0;
-            int pow = 10;   
-            // number from sub-string
-            for(int k=i; k<=j; k++){
-                num = (num*pow) + (arr[k]); 
-            }
-            subArray[count++] = num;
-        }
-    }
-    return count;
-}
 
-// convert number into array
-int addNumToArr(int arr[], int inpNum){
-    // calculate total digits;
-    int num = inpNum;
-    int size = 0;
-    while(num){
-        size++;
-        num /= 10;
-    }
-    // add values in array
-    for(int idx=size-1; idx>=0; idx--){
-        arr[idx] = inpNum%10;
-        inpNum /= 10;
-    }
-    return size;
-}
-
-
-// funtion to find total CSD Number
-void maxCSD(string s, int size)
-{   
-    int numArr[MAX];
-    int subArrays[MAX];
-    int allPossibleCSD[MAX];
-    int count = 0;
-    
-    // convert string int numberai array
-    addNumToArr(numArr, stoi(s));
-
-
-    // store all sub-string numeric values in a array
-    size = getSubArrays(numArr, size, subArrays);
-    
-    // all possible csd & increment count
-    for(int idx=0; idx<=size-1; idx++){
-        if(isCSDNumber(subArrays[idx])){
-            allPossibleCSD[count] = subArrays[idx];
-            count++;
-        }
-    }
-
-    // sort in descreasing order
-    sort(allPossibleCSD, allPossibleCSD+count, sortOrder);
-
-    int maxCSD = count;
-
-    // check CSD should not be sub-string or super-string of any other CSD number.
-    for(int i=0; i<=count-1; i++)
+// function to check if digits of CSD number till now has been a part of any other CSD number
+bool is_valid(bool* visited, int start, int end)
+{
+    for(int idx=start; idx<end; idx++)
     {
-        //convert possible CSD numeric values into array
-        size = addNumToArr(numArr, allPossibleCSD[i]);
-        // get sub-string for ever csd number
-        size = getSubArrays(numArr, size, subArrays);
-        // compare substring with other CSD numbers
-        for(int j=0; j<=size-1; j++)
+        // if digit of CSD number is visted, return false
+        if(visited[idx])
         {
-            for(int k=i+1; k<=count-1; k++){
-                int compare = subArrays[j];
-                if(allPossibleCSD[j] == compare){
-                    maxCSD--;
-                }
-            }
+            return false;
         }
     }
-    cout << "Total CSD Numbers: ";
-    cout << maxCSD;
+    return true;
 }
-
 
 
 // function to drive code
 int main()
-{
+{   
     int size;
-    cout << "Enter string size: ";
-    cin >> size;
-    // cout << isCSDNumber(size);
+    cout << "Enter String Length: ";
+    cin>>size;
+    
+    string s;
+    cout << "Enter String: ";
+    cin>>s;
 
-    string str;
-    cout << "Enter your string: ";
-    cin >> str;
+    long count = 0;
 
-    // max csd numbers that can be formed
-    maxCSD(str, size);
-    cout << endl;
+    // array to store status for CSD number digit
+    bool* visited = new bool[size];
+    
+    // setting default values of visited array
+    for(int i=0; i<s.length(); i++)
+    {
+        visited[i]=false;
+    }
+
+    // loop to maintain Length of substring
+    for(int len=1; len <= s.length(); len++)
+    {   
+        // loop to maintain Start Position of substring
+        for(int start=0; start<=s.length()-len; start++)
+        {   
+            int end = start+len;
+            // find substring using STL funtion
+            string sub = s.substr(start, len);
+
+            // check for CSD number
+            if(is_cb_number(sub) && is_valid(visited,start,end))
+            {
+                count++;
+
+                // update status for CSD number digits
+                for(int k=start; k<end; k++)
+                {   
+                    visited[k]=true;
+                }
+            }
+        }
+    }
+
+    cout << "Maximum number of CB numbers that are formed: ";
+    cout << count << endl;
 
     return 0;
 }
-
 /*
 
 Approach:
-
-    According to the Problem the work is to find the Max CB numbers from the given number.
-
-    As to work on substrings of the given number, we will capture the element into the String instead of integer .
-    The number 'n' given is nothing to do with our approach(as we are working on String) but can be useful acoording to your approach.
-    Intuition :
-    Put loop on string that will give substring of every length.
-    create a function that will return true if the passed number is a CB number otherwise return false.
-    To put a check if the digit is already a part of the any other CB number, create an boolean array say, valid which store which digits till now has been a part of any other CB number.
-    Take a counter and increment if a CB number is found.
-    At the end print the count.
-
-Code:
-
-    #include <iostream>
-    #include <bits/stdc++.h>
-
-    using namespace std;
-
-    long stoint(string s)
-    {
-        long i,j=0;
-        i = 0;
-        while(s[j] >= '0' && s[j] <= '9')
-        {
-            i = i * 10 + (s[j] - '0');
-            j++;
-        }
-        return i;
-    }
-
-    bool is_cb_number(string s)
-    {
-        long long num = stoint(s);
-        if (num == 0 || num == 1) {
-            return false;
-        }
-        int arr[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
-
-            for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-                if (num == arr[i]) {
-                    return true;
-                }
-            }
-
-            for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) {
-                if (num % arr[i] == 0) {
-                    return false;
-                }
-            }
-
-        return true;
-    }
-
-    bool is_valid(bool * visited, int l, int r)
-    {
-        for(int i=l;i<r;i++)
-        {
-            if(visited[i])
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    int main()
-    {
-        int n;
-        cin>>n;
-        string s;
-        cin>>s;
-        long count = 0;
-        bool* visited = new bool[n];
-    for(int i=0; i<s.length(); i++)
-        {
-            visited[i]=false;
-        }
-        for(int i=1; i <= s.length(); i++)
-        {
-            for(int j=0; j<=s.length()-i; j++)
-            {   int e = j+i;
-                string sub = s.substr(j, e-j);
-                if(is_cb_number(sub) && is_valid(visited,j,e))
-                {
-                    count++;
-                    for(int k=j;k<e;k++)
-                    {
-                        visited[k]=true;
-                    }
-                }
-            }
-        }
-        cout<<count<<endl;
-        return 0;
-    }
+    - According to the Problem the work is to find the Max CSD numbers from the given number.
+    - As to work on substrings of the given number, we will capture the element into the String
+      instead of integer .
+    - The number 'n' given is nothing to do with our approach(as we are working on String) but 
+      can be useful acoording to your approach.
+    
+Intuition :
+    - Put loop on string that will give substring of every length.
+    - create a function that will return true if the passed number is a CSD number otherwise return false.
+    - To put a check if the digit is already a part of the any other CSD number, create an boolean array
+      say, valid[] which store which digits till now has been a part of any other CSD number.
+    - Take a counter and increment if a CSD number is found.
+    - At the end print the count.
 
 */
