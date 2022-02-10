afe3603d13bbf75b4d6a1c5a0feec16b6b3c7fc3
Yogesh <yogesh7132@gmail.com>
2022-02-10 11:10:42 +0530

    Updated comments

--- 18_recursion_III_quick_thinking/04_tiling_problem_recursion.cpp
+++ 18_recursion_III_quick_thinking/04_tiling_problem_recursion.cpp
@@ -14,12 +14,21 @@
 
                  wall                      tile-1            tile-2
 
-    Eg: Input   : n = 4
-        Output  : 5
-        Explanation:  For a 2 x 4 wall, there are 5 ways
-                      All 4 vertical (1 way)
-                      All 4 horizontal (1 way)
-                      2 vertical and 2 horizontal (3 ways)
+    Eg: Input   : n = 5
+        Output  : 3
+        Explanation:  For a [4 x 5] wall, there are 3 ways:
+                      - All 5 vertical tiles (1 way)
+                      - 1 vertical and 4 horizontal (1 ways)
+                      - 4 horizontal and 1 vertical (1 ways)
+                        _________           _________                _________  
+                     4 | | | | | |       4 |_______| |            4 | |_______| 
+                       | | | | | |         |_______| |              | |_______| 
+                       | | | | | |         |_______| |              | |_______| 
+                       |_|_|_|_|_|         |_______|_|              |_|_______| 
+                                N=5                 N=5                      N=5
+                        
+                     5-vertical tiles   4-horizontal & 1-vertical  1-vertical & 4-horizontal
+
 */
 
 #include <iostream>
@@ -38,7 +47,23 @@ int getNoOfWays(int n)
     int y = getNoOfWays(n-4);
 
     return x+y;
+    /* 
+        APPROACH:
 
+        When we place a tile of size [4x1] on wall [4xN], then the pending space is n-1
+        Similarly, When we place a tile of size [1x4] on wall [4xN], then the pending space is n-4
+        Eg: 
+                    ______________________________            ______________________________     
+                 4 | |                            |        4 |____|                         |    
+                   | |                            |          |____|                         |    
+                   | |                            |          |____|                         |    
+                   |_|____________________________|          |____|_________________________|    
+                                                 N                                         N     
+                   <-><--------------------------->           <--> <------------------------>    
+                    1              N-1                         4              N-4                
+        
+        Thus, f(n) = f(n-1) + f(n-4)
+    */
 }
 
 
@@ -70,4 +95,8 @@ Case 2:
 Case 3:
     Enter wall width [4 x N]: 5
     Number ways to build wall: 3
+
+Case 4:
+    Enter wall width [4 x N]: 6
+    Number ways to build wall: 4
 */
