153496d54d608d084c707fdcb101154565a6661f
Yogesh <yogesh7132@gmail.com>
2022-05-06 05:10:01 +0530

    Add Stacks

---
+++ 27_stacks/01_stack_introduction.cpp
@@ -0,0 +1,18 @@
+/*
+    TOPIC: Stack - Introduction
+
+    Stack is data structure which maintains information in the LIFO order.
+    LIFO -> Last In First Out
+
+    Stack can be inplemented using Array,
+                             using Linked List,
+                             using Vector
+
+    Important operation in stack:
+    - Push (Insertion of data)
+    - Pop  (Removal of data)
+    - Top  (viewing the topmost element)
+
+    - isEmpty() (check if stack is empty)
+    - isFull()  (check if stack is full)
+*/
